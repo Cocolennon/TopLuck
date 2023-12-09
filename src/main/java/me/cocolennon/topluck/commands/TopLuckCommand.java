48722bfeb9af50a1876b47d5b1471b129e3ba1eb
Cocolennon <cocolennong@gmail.com>
2023-12-09 20:18:03 +0100

    Added info and menu subcommand, no arguments or random arguments will still open the menu

--- src/main/java/me/cocolennon/topluck/commands/TopLuckCommand.java
+++ src/main/java/me/cocolennon/topluck/commands/TopLuckCommand.java
@@ -1,26 +1,69 @@
 package me.cocolennon.topluck.commands;
 
+import me.cocolennon.topluck.Main;
 import me.cocolennon.topluck.util.MenuCreator;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class TopLuckCommand implements CommandExecutor {
+public class TopLuckCommand implements TabExecutor {
+    private static final List<String> autoComplete = Arrays.asList("info", "menu");
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(!(sender instanceof Player)) return false;
+
+        if(args.length == 0) return openMenu(sender);
+        return switch (args[0]) {
+            case "menu" -> openMenu(sender);
+            case "info" -> sendInfo(sender);
+            default -> openMenu(sender);
+        };
+    }
+
+    @Nullable @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(!(sender instanceof Player)) return null;
+        if(args.length == 1) return autoComplete;
+        return null;
+    }
+
+    private boolean openMenu(CommandSender sender) {
         if(!sender.hasPermission("topluck.open")) {
-            sender.sendMessage("§c[Top Luck] You can't do that!");
+            sender.sendMessage("§a[Top Luck] §cYou can't do that!");
             return false;
         }
         List<Inventory> inventories = new LinkedList<>(MenuCreator.getInstance().getPages());
         Player player = (Player) sender;
         player.openInventory(inventories.get(0));
+        return true;
+    }
+
+    private boolean sendInfo(CommandSender sender) {
+        if (!sender.hasPermission("topluck.info")) {
+            sender.sendMessage("§a[Top Luck] §cYou can't do that!");
+            return false;
+        }
+        List<String> info = new LinkedList<>();
+        info.add("§c§l=========================");
+        info.add("§a§lTop Luck " + Main.getInstance().getVersion());
+        if(Main.getInstance().getUsingOldVersion()){
+            info.add("§aAn update is available!");
+        }else{
+            info.add("§aYou're using the latest version");
+        }
+        info.add("§aMade with §c❤ §aby §d§lCoco Lennon");
+        info.add("§c§l=========================");
+
+        info.forEach(sender::sendMessage);
         return true;
     }
 }
