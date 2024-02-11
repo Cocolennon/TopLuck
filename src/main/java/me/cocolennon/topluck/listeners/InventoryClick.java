5fb749d0f0ca8f9067a65963046b0ed17dc4333e
Cocolennon <cocolennong@gmail.com>
2024-02-11 13:04:54 +0100

    Removed unused import

--- src/main/java/me/cocolennon/topluck/listeners/InventoryClick.java
+++ src/main/java/me/cocolennon/topluck/listeners/InventoryClick.java
@@ -11,7 +11,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
