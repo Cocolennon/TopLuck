50b6030142a5bf2989d86db5fcb218cfe63191fc
Cocolennon <cocolennong@gmail.com>
2024-03-30 17:18:58 +0100

    Added listener for Player Quit event and made it save the player's inventory

--- src/main/java/me/cocolennon/topluck/listeners/PlayerJoined.java
+++ src/main/java/me/cocolennon/topluck/listeners/PlayerJoined.java
@@ -1,10 +1,12 @@
 package me.cocolennon.topluck.listeners;
 
 import me.cocolennon.topluck.Main;
+import me.cocolennon.topluck.util.PlayerData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerJoined implements Listener {
     private final String topluckColor = Main.getInstance().getConfig().getString("plugin-name-color");
@@ -18,5 +20,10 @@ public class PlayerJoined implements Listener {
         if(Main.getInstance().getUsingOldVersion()) {
             player.sendMessage(topluckColor + "[Top Luck]" + successColor + " You are using an older version of Top Luck, please update to version " + Main.getInstance().getLatestVersion());
         }
+    }
+
+    @EventHandler
+    public void PlayerLeave(PlayerQuitEvent event) {
+        PlayerData.getInstance().savePlayerInventory(event.getPlayer());
     }
 }
